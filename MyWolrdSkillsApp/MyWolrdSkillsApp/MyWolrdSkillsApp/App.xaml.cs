04f3b9a490163e5694ab71b25bafefa2e71e4f25
LucasFelicianoAlmeida <lucasfeliciano326@gmail.com>
2021-02-07 07:56:34 -0300

    .add first file

---
+++ MyWolrdSkillsApp/MyWolrdSkillsApp/MyWolrdSkillsApp/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using MyWolrdSkillsApp.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyWolrdSkillsApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new PrincipalPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
